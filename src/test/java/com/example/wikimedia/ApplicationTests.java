749a4674241f2ff267f5a3e23b68efc1dfb67d71
gyandev <gyan123edu@gmail.com>
2022-09-13 12:01:08 +0530

    "first commit"

---
+++ src/test/java/com/example/wikimedia/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wikimedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
